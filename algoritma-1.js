2368fdb881b6fa95369a0f12c5aaf78a7fa20631
daniellapapple <124172120+daniellapapple@users.noreply.github.com>
2023-02-01 23:06:34 +0700

    Create algoritma-1.js

---
+++ algoritma-1.js
@@ -0,0 +1,10 @@
+const string = 'NEGIE1'
+const split = string.split('')
+const reverse = split.reverse()
+const splice = reverse.splice(0, 1)
+
+reverse.push('1)
+
+const join = reverse.join('')
+
+console.log(join) // Hasil nya reverse dari 'NEGIE1'
